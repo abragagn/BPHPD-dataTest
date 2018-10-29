c4567c397b5ca9568ac36fc18b5361e9737a007a
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2018-10-29 11:25:24 +0100

    Fix 2017 scripts

--- ntuples/2017/l_clean.sh
+++ ntuples/2017/l_clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ch2017B; rm png; rm -r s*; cd ..;
-cd ch2017C; rm png; rm -r s*; cd ..;
-cd ch2017D; rm png; rm -r s*; cd ..;
-cd ch2017E; rm png; rm -r s*; cd ..;
-cd ch2017F; rm png; rm -r s*; cd ..;
+cd ch2017B; rm *png; rm -r s*; cd ..;
+cd ch2017C; rm *png; rm -r s*; cd ..;
+cd ch2017D; rm *png; rm -r s*; cd ..;
+cd ch2017E; rm *png; rm -r s*; cd ..;
+cd ch2017F; rm *png; rm -r s*; cd ..;
