99eb3db02d158a0042dfe222087687a7981ccbbf
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2018-10-29 11:33:30 +0100

    Fix 2018 scripts

--- ntuples/2018/l_fit.sh
+++ ntuples/2018/l_fit.sh
@@ -7,4 +7,4 @@ root -l -q -b 'fitBs18.C("ntu2018Bv2.root")';
 root -l -q -b 'fitBs18.C("ntu2018Cv1.root")';
 root -l -q -b 'fitBs18.C("ntu2018Cv2.root")';
 root -l -q -b 'fitBs18.C("ntu2018Cv3.root")';
-root -l -q -b 'fitBs18.C("ntu2018Dv1.root")';
+root -l -q -b 'fitBs18.C("ntu2018Dv2.root")';
